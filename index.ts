b75248c6fe688ebaa765505550444b505c036794
Biswarup35 <biswarup35@gmail.com>
2022-02-04 14:01:34 +0530

    adding cors

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import recipes from "./src/recipes";
 import recipeId from "./src/recipeId";
 
 const app = express();
 const PORT = process.env.PORT ?? 5000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
